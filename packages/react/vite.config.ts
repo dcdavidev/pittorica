2e0dc0a91ace108cd82e2d548e7575b061537d1f
Davide Di Criscito <davidedc@outlook.com>
2025-11-16 19:29:59 +0100

    feat(react): init

---
+++ packages/react/vite.config.ts
@@ -0,0 +1,37 @@
+/// <reference types='vitest' />
+import path from 'node:path';
+
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig(() => ({
+  root: import.meta.dirname,
+  cacheDir: '../../node_modules/.vite/packages/react',
+  plugins: [
+    dts({
+      entryRoot: 'src',
+      tsconfigPath: path.join(import.meta.dirname, 'tsconfig.lib.json'),
+    }),
+  ],
+  build: {
+    outDir: './dist',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    lib: {
+      // Could also be a dictionary or array of multiple entry points.
+      entry: 'src/index.ts',
+      name: '@pittorica/react',
+      fileName: 'index',
+      // Change this to the formats you want to support.
+      // Don't forget to update your package.json as well.
+      formats: ['es' as const, 'cjs' as const],
+    },
+    rollupOptions: {
+      // External packages that should not be bundled into your library.
+      external: [],
+    },
+  },
+}));
--- pittorica/README.md
+++
@@ -1,7 +0,0 @@
-# pittorica
-
-This library was generated with [Nx](https://nx.dev).
-
-## Building
-
-Run `nx build pittorica` to build the library.
