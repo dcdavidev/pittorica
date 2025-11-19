260e7bb10dab5a4222e7fb2e7f1bb6264274b20f
Davide Di Criscito <davidedc@outlook.com>
2025-11-19 14:30:18 +0100

    refactor(picto): reset lib

---
+++ packages/picto/README.md
@@ -0,0 +1,7 @@
+# @pittorica/picto
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @pittorica/picto` to execute the unit tests via [Vitest](https://vitest.dev/).
---
+++ packages/picto/vite.config.ts
@@ -0,0 +1,38 @@
+/// <reference types='vitest' />
+import path from 'node:path';
+
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(() => ({
+  root: import.meta.dirname,
+  cacheDir: '../../node_modules/.vite/packages/picto',
+  plugins: [
+    react(),
+    dts({
+      entryRoot: 'src',
+      tsconfigPath: path.join(import.meta.dirname, 'tsconfig.lib.json'),
+    }),
+    vanillaExtractPlugin(),
+  ],
+  build: {
+    outDir: './dist',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    lib: {
+      // Could also be a dictionary or array of multiple entry points.
+      entry: 'src/index.ts',
+      name: '@pittorica/picto',
+      fileName: 'index',
+      formats: ['es' as const],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+    },
+  },
+}));
