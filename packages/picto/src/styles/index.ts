e1188ddfa54e6d89e45c321d2786c57c9c5f0a45
Davide Di Criscito <dcdavidev@gmail.com>
2025-11-21 14:23:33 +0100

    fix(picto): rearrange exports

--- packages/picto/src/styles/index.ts
+++ packages/picto/src/styles/index.ts
@@ -1,6 +1,6 @@
 export * from './color-utils.js';
 export * from './global.css.js';
 export * from './sprinkles.css.js';
-export * from './theme-utils.js';
 export * from './theme.css.js';
+export * from './theme-utils.js';
 export * from './tokens.js';
